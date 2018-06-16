a0cd958c73b1d978866444c85696ab86377e3c9f
foibuls <drhearobinson@gmail.com>
2018-06-15 22:17:47 -0700

    pregradlechange

---
+++ desktop/src/com/mygdx/newbrain/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.newbrain.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.newbrain.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
